51c55f9c381e5250d8d8c5d0d34e402bae642683
camila <ccamanio@dc.uba.ar>
2022-02-11 22:11:30 -0300

    first commit

---
+++ src/main/java/com/example/configserverfinal/ConfigServerFinalApplication.java
@@ -0,0 +1,15 @@
+package com.example.configserverfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/configserverfinal/ConfigServerFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserverfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
